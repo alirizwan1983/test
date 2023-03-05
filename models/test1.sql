f47dcf67ce83549e8f1cbafcfe05bbf7a60031af
ideaclouddataplatformclinicalengineeringazure@adventhealth.com <ideaclouddataplatformclinicalengineeringazure@adventhealth.com>
2023-03-05 04:16:06 +0000

    update date

--- models/test1.sql
+++ models/test1.sql
@@ -9,4 +9,4 @@ union
 select 3 as id, 'cust3' as custname, '2023-03-03' as loaddate
 union
 
-select 4 as id, 'cust4' as custname, '2023-03-03' as loaddate
+select 4 as id, 'cust4' as custname, '2023-03-04' as loaddate
