9ba5970fec686773837e03fc4ed0a85b7f609014
ideaclouddataplatformclinicalengineeringazure@adventhealth.com <ideaclouddataplatformclinicalengineeringazure@adventhealth.com>
2023-03-05 04:15:29 +0000

    add cust

--- models/test1.sql
+++ models/test1.sql
@@ -7,3 +7,6 @@ select 2 as id, 'cust2' as custname, '2023-03-02' as loaddate
 union
 
 select 3 as id, 'cust3' as custname, '2023-03-03' as loaddate
+union
+
+select 4 as id, 'cust4' as custname, '2023-03-03' as loaddate
